702e22120b939aad924de322aae281d8214ebc31
Garrick Fernandez <20538779+garrickf@users.noreply.github.com>
2020-06-07 00:39:11 -0700

    add socket cxns on backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
-/** Server code (adapted from MDN web docs) */
-// Load HTTP module
-const http = require("http");
-const HOSTNAME = "127.0.0.1";
-const PORT = 8080;
+/** server.js
+ * 
+ * The central log server accepts connections that stream it log data, as well
+ * as serving the frontend.
+ * 
+ * Sources:
+ * MDN web docs: TODO: get link
+ */
+
+const http = require("http"); // Load HTTP module
+const HOSTNAME = "localhost";
+const SERVE_PORT = 6006;
+
+const net = require("net"); // For lower-lever management of sockets
+const readline = require("readline");
+
+const Parser = require("./parser");
 
 // Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
@@ -13,8 +25,41 @@ const server = http.createServer((req, res) => {
 });
 
 // Listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(SERVE_PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${SERVE_PORT}/`);
+});
+
+// Listen for client connections on another port, these are streaming data to us
+const logServer = net
+  .createServer((socket) => {
+    const readInterface = readline.createInterface({
+      input: socket,
+      output: socket,
+      console: false,
+    });
+
+    // Create parser around read interface
+    const parser = new Parser(readInterface);
+    parser.onLogEvent((event) => {
+      console.log(event);
+    })
+    parser.start();
+
+    socket.addListener("connect", function () {
+      console.log("client connected: " + this.remoteAddress);
+    });
+
+    // TODO: add parser to a combined utility that cross-references and combines
+    // log events into spans
+  })
+  .on("error", (err) => {
+    // Handle errors here.
+    throw err;
+  });
+
+// Grab an arbitrary unused port.
+logServer.listen(7007, HOSTNAME, () => {
+  console.log("opened server on", logServer.address());
 });
 
 // TODO: make server listen for stream connections, take logs and parse them
