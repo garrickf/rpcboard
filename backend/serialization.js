34ec8db124ee123a4112ef67057285fc619303d0
Garrick Fernandez <20538779+garrickf@users.noreply.github.com>
2020-06-11 20:44:13 -0700

    clean up serialization

--- backend/serialization.js
+++ backend/serialization.js
@@ -1,49 +1,34 @@
+/** Utilities for serialization and deserialization (from JSON) */
+
 const Timestamp = require("./classes/timestamp");
 const TreeNode = require("./classes/treeNode");
 const Span = require("./classes/span");
 const LogEvent = require("./classes/logEvent");
-const { TRANSPORT_OPS } = require("./constants");
 
-// TODO: pretty messy, and wrong nouns used. Clean this up!
-
-const logEventFromJSON = (JSONLogEvent) => {
-  // const data = JSON.parse(JSONLogEvent, (key, value) => {
-  //   if (TRANSPORT_OPS.hasOwnProperty(key)) {
-  //     return new Timestamp(value);
-  //   } else {
-  //     return value;
-  //   }
-  // });
-  Object.keys(JSONLogEvent.opTimestamps).forEach((key) => {
-    const ts_string = JSONLogEvent.opTimestamps[key];
-    JSONLogEvent.opTimestamps[key] = new Timestamp(ts_string);
+/** Creates a live LogEvent (member functions added) from a passed in object */
+const logEventFromObj = (logEvent_obj) => {
+  Object.keys(logEvent_obj.opTimestamps).forEach((key) => {
+    const ts_string = logEvent_obj.opTimestamps[key];
+    logEvent_obj.opTimestamps[key] = new Timestamp(ts_string);
   });
   const logEvent = new LogEvent();
-  logEvent.addData(JSONLogEvent);
+  logEvent.addData(logEvent_obj);
   return logEvent;
 };
 
-/** Passed in an object (for reviving) */
-const spanFromJSON = (JSONSpan) => {
-  //   const data = JSON.parse(JSONSpan, (key, value) => {
-  //   if (key === "clientEvent" || key === "serverEvent") {
-  //     console.log(value);
-  //     return logEventFromJSON(value);
-  //   } else {
-  //     return value;
-  //   }
-  // });
-
-  // const span = new Span(data.clientEvent, data.serverEvent);
+/** Creates a live Span (member functions added) from a passed in object */
+const spanFromObj = (span_obj) => {
   // TODO: allow constructor to take undefined, undefined to keep pattern consistent
   const span = new Span(
-    logEventFromJSON(JSONSpan.clientEvent),
-    logEventFromJSON(JSONSpan.serverEvent)
+    logEventFromObj(span_obj.clientEvent),
+    logEventFromObj(span_obj.serverEvent)
   );
-  // span.addData(data);
+  span.setClientNodeId(span_obj.clientNodeId);
+  span.setServerNodeId(span_obj.serverNodeId);
   return span;
 };
 
+/** Recursively turns objects into live TreeNodes */
 const turnIntoTree = (data) => {
   const treeNode = new TreeNode();
   treeNode.addData(data);
@@ -54,11 +39,13 @@ const turnIntoTree = (data) => {
 };
 
 const treeFromJSON = (JSONTree) => {
+  console.log(JSONTree); // Debug
+
   // NOTE: second parameter is reviver function, takes completed object!
   const data = JSON.parse(JSONTree, (key, value) => {
     if (key === "span") {
       if (!value) return value;
-      return spanFromJSON(value);
+      return spanFromObj(value);
     } else {
       return value;
     }
@@ -67,7 +54,30 @@ const treeFromJSON = (JSONTree) => {
   return turnIntoTree(data);
 };
 
+const hTreeFromObj = (hTree_obj) => {
+  // console.log(hTree_obj); // Debug
+  // Recursively reconstitute spans
+  const reviveSpans = (obj) => {
+    obj.children.forEach((child) => {
+      reviveSpans(child);
+    });
+    if (!!obj.span) {
+      obj.span = spanFromObj(obj.span);
+    }
+  };
+  reviveSpans(hTree_obj);
+  return turnIntoTree(hTree_obj);
+};
+
+const timestampsFromNSStringArray = (timestamps) => {
+  return timestamps.map((timestampString) => {
+    return new Timestamp(timestampString);
+  });
+};
+
 // Usage: const { treeFromJSON } = require("./serialization");
 module.exports = {
   treeFromJSON,
+  hTreeFromObj,
+  timestampsFromNSStringArray,
 };
